4d387faa3a9323514bd4bececb49b371c01564b7
Ricardo Vieira dos Santos <gestricardo22@gmail.com>
2025-03-31 09:37:02 -0300

    Adiciona a classe principal do aplicativo de despesas com estrutura inicial e tela inicial

--- despesas/lib/main.dart
+++ despesas/lib/main.dart
@@ -1,1 +1,26 @@
+import 'package:flutter/material.dart';
 
+class ExpensesApp extends StatelessWidget {
+  const ExpensesApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Despesas',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Text('versao inicial'));
+  }
+}
