545d3e648e05b3c96ee6d324a6130690ea766961
Ricardo Vieira dos Santos <gestricardo22@gmail.com>
2025-04-03 20:06:50 -0300

    Substitui a exibição da lista de transações por um componente TransactionList

--- despesas/lib/components/transaction_list.dart
+++ despesas/lib/components/transaction_list.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import '../../model/transaction.dart';
+import 'package:intl/intl.dart';
 
 class TransactionList extends StatelessWidget {
   final List<Transaction> transactions;
@@ -7,6 +8,59 @@ class TransactionList extends StatelessWidget {
   const TransactionList(this.transactions, {super.key});
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      children:
+          transactions.map((tr) {
+            return Card(
+              child: Row(
+                children: <Widget>[
+                  Container(
+                    margin: const EdgeInsets.symmetric(
+                      vertical: 10,
+                      horizontal: 15,
+                    ),
+                    decoration: BoxDecoration(
+                      //border: Border.all(color: Colors.purple, width: 2),
+                    ),
+                    padding: const EdgeInsets.all(10),
+                    child: Text(
+                      //Interpolando String no valor da transação
+                      'R\$ ${tr.value.toStringAsFixed(2)}',
+                      style: const TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.purple,
+                      ),
+                    ),
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        tr.title,
+                        style: const TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text(
+                        tr.category,
+                        style: const TextStyle(
+                          color: Color.fromARGB(255, 26, 26, 26),
+                        ),
+                      ),
+                      Text(
+                        DateFormat('dd/MM/yyy').format(tr.date),
+                        style: const TextStyle(
+                          color: Color.fromARGB(255, 56, 56, 56),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            );
+          }).toList(),
+    );
   }
 }
--- despesas/lib/main.dart
+++ despesas/lib/main.dart
@@ -1,6 +1,6 @@
+import 'package:despesas/components/transaction_list.dart';
 import 'package:flutter/material.dart';
 import './model/transaction.dart';
-import 'package:intl/intl.dart';
 import 'package:flutter/services.dart';
 
 main() => runApp(ExpensesApp());
@@ -91,60 +91,7 @@ class MyHomePage extends StatelessWidget {
             ),
           ),
           //Espaço entre o gráfico e a lista de transações
-          Column(
-            children:
-                _transactions.map((tr) {
-                  return Card(
-                    child: Row(
-                      children: <Widget>[
-                        Container(
-                          margin: const EdgeInsets.symmetric(
-                            vertical: 10,
-                            horizontal: 15,
-                          ),
-                          decoration: BoxDecoration(
-                            //border: Border.all(color: Colors.purple, width: 2),
-                          ),
-                          padding: const EdgeInsets.all(10),
-                          child: Text(
-                            //Interpolando String no valor da transação
-                            'R\$ ${tr.value.toStringAsFixed(2)}',
-                            style: const TextStyle(
-                              fontSize: 20,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.purple,
-                            ),
-                          ),
-                        ),
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              tr.title,
-                              style: const TextStyle(
-                                fontSize: 18,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                            Text(
-                              tr.category,
-                              style: const TextStyle(
-                                color: Color.fromARGB(255, 26, 26, 26),
-                              ),
-                            ),
-                            Text(
-                              DateFormat('dd/MM/yyy').format(tr.date),
-                              style: const TextStyle(
-                                color: Color.fromARGB(255, 56, 56, 56),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                  );
-                }).toList(),
-          ),
+          TransactionList(_transactions),
           Card(
             elevation: 8,
             child: Padding(
