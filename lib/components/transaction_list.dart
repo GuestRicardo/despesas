aefc263b5d50269b530cea57cac92f724a13005c
Ricardo Vieira dos Santos <gestricardo22@gmail.com>
2025-04-07 17:00:34 -0300

    Refatora a exibição da lista de transações para usar ListView.builder, melhorando a performance e a legibilidade do código

--- lib/components/transaction_list.dart
+++ lib/components/transaction_list.dart
@@ -10,59 +10,63 @@ class TransactionList extends StatelessWidget {
   Widget build(BuildContext context) {
     return SizedBox(
       height: 500,
-      child: ListView(
-        children:
-            transactions.map((tr) {
-              return Card(
-                child: Row(
-                  children: <Widget>[
-                    Container(
-                      margin: const EdgeInsets.symmetric(
-                        vertical: 10,
-                        horizontal: 15,
-                      ),
-                      decoration: BoxDecoration(
-                        //border: Border.all(color: Colors.purple, width: 2),
-                      ),
-                      padding: const EdgeInsets.all(10),
-                      child: Text(
-                        //Interpolando String no valor da transação
-                        'R\$ ${tr.value.toStringAsFixed(2)}',
-                        style: const TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.purple,
-                        ),
+      child: ListView.builder(
+        itemCount: transactions.length,
+        itemBuilder: (context, index) {
+          final tr = transactions[index];
+          //return Card(
+          //  child: Text(tr.title),
+          //);
+          return Card(
+            child: Row(
+              children: <Widget>[
+                Container(
+                  margin: const EdgeInsets.symmetric(
+                    vertical: 10,
+                    horizontal: 15,
+                  ),
+                  decoration: BoxDecoration(
+                    //border: Border.all(color: Colors.purple, width: 2),
+                  ),
+                  padding: const EdgeInsets.all(10),
+                  child: Text(
+                    //Interpolando String no valor da transação
+                    'R\$ ${tr.value.toStringAsFixed(2)}',
+                    style: const TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.purple,
+                    ),
+                  ),
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      tr.title,
+                      style: const TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
                       ),
                     ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          tr.title,
-                          style: const TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        Text(
-                          tr.category,
-                          style: const TextStyle(
-                            color: Color.fromARGB(255, 26, 26, 26),
-                          ),
-                        ),
-                        Text(
-                          DateFormat('dd/MM/yyy').format(tr.date),
-                          style: const TextStyle(
-                            color: Color.fromARGB(255, 56, 56, 56),
-                          ),
-                        ),
-                      ],
+                    Text(
+                      tr.category,
+                      style: const TextStyle(
+                        color: Color.fromARGB(255, 26, 26, 26),
+                      ),
+                    ),
+                    Text(
+                      DateFormat('dd/MM/yyy').format(tr.date),
+                      style: const TextStyle(
+                        color: Color.fromARGB(255, 56, 56, 56),
+                      ),
                     ),
                   ],
                 ),
-              );
-            }).toList(),
+              ],
+            ),
+          );
+        },
       ),
     );
   }
