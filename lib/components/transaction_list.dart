a420433a2ab640a14b0b0b7d64bc9f4f67cef480
Ricardo Vieira dos Santos <gestricardo22@gmail.com>
2025-04-07 16:50:59 -0300

    Refatora a exibição da lista de transações para usar ListView e ajusta o estilo dos textos

--- lib/components/transaction_list.dart
+++ lib/components/transaction_list.dart
@@ -10,61 +10,59 @@ class TransactionList extends StatelessWidget {
   Widget build(BuildContext context) {
     return SizedBox(
       height: 500,
-      child: SingleChildScrollView(
-        child: Column(
-          children:
-              transactions.map((tr) {
-                return Card(
-                  child: Row(
-                    children: <Widget>[
-                      Container(
-                        margin: const EdgeInsets.symmetric(
-                          vertical: 10,
-                          horizontal: 15,
+      child: ListView(
+        children:
+            transactions.map((tr) {
+              return Card(
+                child: Row(
+                  children: <Widget>[
+                    Container(
+                      margin: const EdgeInsets.symmetric(
+                        vertical: 10,
+                        horizontal: 15,
+                      ),
+                      decoration: BoxDecoration(
+                        //border: Border.all(color: Colors.purple, width: 2),
+                      ),
+                      padding: const EdgeInsets.all(10),
+                      child: Text(
+                        //Interpolando String no valor da transação
+                        'R\$ ${tr.value.toStringAsFixed(2)}',
+                        style: const TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.purple,
                         ),
-                        decoration: BoxDecoration(
-                          //border: Border.all(color: Colors.purple, width: 2),
-                        ),
-                        padding: const EdgeInsets.all(10),
-                        child: Text(
-                          //Interpolando String no valor da transação
-                          'R\$ ${tr.value.toStringAsFixed(2)}',
+                      ),
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          tr.title,
                           style: const TextStyle(
-                            fontSize: 20,
+                            fontSize: 18,
                             fontWeight: FontWeight.bold,
-                            color: Colors.purple,
                           ),
                         ),
-                      ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            tr.title,
-                            style: const TextStyle(
-                              fontSize: 18,
-                              fontWeight: FontWeight.bold,
-                            ),
+                        Text(
+                          tr.category,
+                          style: const TextStyle(
+                            color: Color.fromARGB(255, 26, 26, 26),
                           ),
-                          Text(
-                            tr.category,
-                            style: const TextStyle(
-                              color: Color.fromARGB(255, 26, 26, 26),
-                            ),
+                        ),
+                        Text(
+                          DateFormat('dd/MM/yyy').format(tr.date),
+                          style: const TextStyle(
+                            color: Color.fromARGB(255, 56, 56, 56),
                           ),
-                          Text(
-                            DateFormat('dd/MM/yyy').format(tr.date),
-                            style: const TextStyle(
-                              color: Color.fromARGB(255, 56, 56, 56),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                );
-              }).toList(),
-        ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              );
+            }).toList(),
       ),
     );
   }
