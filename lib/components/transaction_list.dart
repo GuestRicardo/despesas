4a4d89bfe2adcf4608b0a6fd0c8b52fa0742f9b0
Ricardo Vieira dos Santos <gestricardo22@gmail.com>
2025-04-16 10:05:08 -0300

    Corrige a exibição de transações e melhora a formatação do código no TransactionList

--- lib/components/transaction_list.dart
+++ lib/components/transaction_list.dart
@@ -10,78 +10,86 @@ class TransactionList extends StatelessWidget {
   Widget build(BuildContext context) {
     return SizedBox(
       height: 500,
-      child:tractions.isEmpty ? Column(
-        children:<Widget>[
-         Text(
-         'Nenhuma Transação Cadastrada!',
-          style: Theme.of(context).textTheme.title,
-       ),
-     Container(
-       height:200,
-       child: Image.asset('assets/images/waiting.png',
-       fit:BoxFit.cover,
-     ),
- 
-       ),
-       ],
-       ) : ListView.builder(
-        itemCount: transactions.length,
-        itemBuilder: (context, index) {
-          final tr = transactions[index];
-          //return Card(
-          //  child: Text(tr.title),
-          //);
-          return Card(
-            child: Row(
-              children: <Widget>[
-                Container(
-                  margin: const EdgeInsets.symmetric(
-                    vertical: 10,
-                    horizontal: 15,
-                  ),
-                  decoration: BoxDecoration(
-                    //border: Border.all(color: Colors.purple, width: 2),
-                  ),
-                  padding: const EdgeInsets.all(10),
-                  child: Text(
-                    //Interpolando String no valor da transação
-                    'R\$ ${tr.value.toStringAsFixed(2)}',
-                    style: const TextStyle(
+      child:
+          transactions.isEmpty
+              ? Column(
+                children: <Widget>[
+                  Text(
+                    'Nenhuma Transação Cadastrada!',
+                    style: // Theme.of(context).textTheme.titleLarge,
+                        const TextStyle(
                       fontSize: 20,
                       fontWeight: FontWeight.bold,
-                      color: Colors.purple,
+                      color: Color.fromARGB(255, 255, 255, 255),
                     ),
                   ),
-                ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      tr.title,
-                      style: const TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold,
-                      ),
+                  SizedBox(
+                    height: 200,
+                    child: Image.asset(
+                      'assets/images/waiting.png',
+                      fit: BoxFit.cover,
                     ),
-                    Text(
-                      tr.category,
-                      style: const TextStyle(
-                        color: Color.fromARGB(255, 26, 26, 26),
-                      ),
+                  ),
+                ],
+              )
+              : ListView.builder(
+                itemCount: transactions.length,
+                itemBuilder: (context, index) {
+                  final tr = transactions[index];
+                  //return Card(
+                  //  child: Text(tr.title),
+                  //);
+                  return Card(
+                    child: Row(
+                      children: <Widget>[
+                        Container(
+                          margin: const EdgeInsets.symmetric(
+                            vertical: 10,
+                            horizontal: 15,
+                          ),
+                          decoration: BoxDecoration(
+                            //border: Border.all(color: Colors.purple, width: 2),
+                          ),
+                          padding: const EdgeInsets.all(10),
+                          child: Text(
+                            //Interpolando String no valor da transação
+                            'R\$ ${tr.value.toStringAsFixed(2)}',
+                            style: const TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.purple,
+                            ),
+                          ),
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              tr.title,
+                              style: const TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            Text(
+                              tr.category,
+                              style: const TextStyle(
+                                color: Color.fromARGB(255, 26, 26, 26),
+                              ),
+                            ),
+                            Text(
+                              DateFormat('dd/MM/yyy').format(tr.date),
+                              style: const TextStyle(
+                                color: Color.fromARGB(255, 56, 56, 56),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
                     ),
-                    Text(
-                      DateFormat('dd/MM/yyy').format(tr.date),
-                      style: const TextStyle(
-                        color: Color.fromARGB(255, 56, 56, 56),
-                      ),
-                    ),
-                  ],
-                ),
-              ],
-            ),
-          );
-        },
-      ),
+                  );
+                },
+              ),
     );
   }
 }
