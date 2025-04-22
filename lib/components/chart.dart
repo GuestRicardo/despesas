d51a96bdcd2bdc0fdcbd4603535ec78a7bfef2b1
Ricardo Vieira dos Santos <gestricardo22@gmail.com>
2025-04-22 16:39:49 -0300

    adiciona a lógica para agrupar valores de transações e atualiza o método build do componente Chart

--- lib/components/chart.dart
+++ lib/components/chart.dart
@@ -1,12 +1,27 @@
-
-
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class Chart extends StatelessWidget {
   const Chart({super.key});
 
+  List<Map<String, Object>> get groupedTransactionValues {
+    return List.generate(7, (index) {
+      final weekDay = DateTime.now().subtract(Duration(days: index));
+      double totalSum = 0.0;
+
+      return {'day': DateFormat.E().format(weekDay)[0], 'value': totalSum};
+    }).reversed.toList();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Card(
+      elevation: 6,
+      margin: const EdgeInsets.all(20),
+      child: Row(children: <Widget>[
+
+        ],
+      ),
+    );
   }
 }
