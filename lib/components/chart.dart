3ccf2e49abee492377b857028fdb61e4e0effa60
Ricardo Vieira dos Santos <gestricardo22@gmail.com>
2025-04-22 16:52:39 -0300

    adiciona lógica para calcular a soma total de transações por dia na classe Chart

--- lib/components/chart.dart
+++ lib/components/chart.dart
@@ -3,12 +3,22 @@ import 'package:intl/intl.dart';
 import '../model/transaction.dart';
 
 class Chart extends StatelessWidget {
-  const Chart({super.key});
+  
+  final List<Transaction> recentTransactions;
+  Chart(this.recentTransactions);
 
   List<Map<String, Object>> get groupedTransactionValues {
     return List.generate(7, (index) {
       final weekDay = DateTime.now().subtract(Duration(days: index));
       double totalSum = 0.0;
+
+      for(var i=0; i < recentTransactions.length; i++) {
+        if(recentTransactions[i].date.day == weekDay.day &&
+           recentTransactions[i].date.month == weekDay.month &&
+           recentTransactions[i].date.year == weekDay.year) {
+          totalSum += recentTransactions[i].value;
+        }
+      }
 
       return {'day': DateFormat.E().format(weekDay)[0], 'value': totalSum};
     }).reversed.toList();
