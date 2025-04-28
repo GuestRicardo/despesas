7b8ac8eeec4d0887a871b4bb8a7b8c917f017ba5
Ricardo Vieira dos Santos <gestricardo22@gmail.com>
2025-04-28 14:34:21 -0300

    acessa o método groupedTransactionValues no método build da classe Chart

--- lib/components/chart.dart
+++ lib/components/chart.dart
@@ -31,6 +31,7 @@ class Chart extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    groupedTransactionValues;
     return Card(
       elevation: 6,
       margin: const EdgeInsets.all(20),
