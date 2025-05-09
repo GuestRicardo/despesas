79dabd8aa3b741b28b3e9611f39af0c133504ff1
Ricardo Vieira dos Santos <gestricardo22@gmail.com>
2025-05-09 16:07:19 -0300

    atualiza a formatação das fontes no TransactionList para melhorar a legibilidade

--- lib/components/transaction_list.dart
+++ lib/components/transaction_list.dart
@@ -57,7 +57,7 @@ class TransactionList extends StatelessWidget {
                           child: Text(
                             'R\$ ${tr.value.toStringAsFixed(2)}',
                             style: const TextStyle(
-                              fontSize: 20,
+                              fontSize: 25,
                               fontWeight: FontWeight.bold,
                             ),
                           ),
@@ -67,7 +67,7 @@ class TransactionList extends StatelessWidget {
                     title: Text(
                       tr.title,
                       style: const TextStyle(
-                        fontSize: 25,
+                        fontSize: 30,
                         fontWeight: FontWeight.bold,
                         color: Color.fromARGB(255, 255, 255, 255),
                       ),
@@ -75,6 +75,7 @@ class TransactionList extends StatelessWidget {
                     subtitle: Text(
                       DateFormat('dd/MM/yyy').format(tr.date),
                       style: const TextStyle(
+                        fontSize: 20,
                         color: Color.fromARGB(255, 255, 255, 255),
                       ),
                     ),
