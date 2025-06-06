9e2111b6b5677314ea454581508aeeb4e11a88cd
Ricardo Vieira dos Santos <gestricardo22@gmail.com>
2025-06-06 13:21:53 -0300

    aumenta a altura do widget TransactionList para melhorar a exibição

--- lib/components/transaction_list.dart
+++ lib/components/transaction_list.dart
@@ -10,7 +10,7 @@ class TransactionList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      height: 500,
+      height: 700,
       child:
           transactions.isEmpty
               ? Column(
