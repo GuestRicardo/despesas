ac3bbd33f2d3f59f4182e52b0991afd91d847e03
Ricardo Vieira dos Santos <gestricardo22@gmail.com>
2025-06-11 21:03:53 -0300

    ajusta o layout do widget Chart para utilizar SizedBox e melhora a responsividade

--- lib/main.dart
+++ lib/main.dart
@@ -181,7 +181,7 @@ class _MyHomePageState extends State<MyHomePage> {
             //   ),
             // ),
             Chart(_recentTransactions),
-
+            //Lista de transações
             TransactionList(_transactions, _removeTransaction),
             //Espaço entre o gráfico e a lista de transações
           ],
